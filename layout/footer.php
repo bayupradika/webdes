ec93580e41b735ef3d16d95a307a5f3449d28f65
WalkerVz <ravilmuhammad987@gmail.com>
2023-07-24 16:09:24 +0700

    desa

--- layout/footer.php
+++ layout/footer.php
@@ -6,7 +6,7 @@
       <div class="rowflex">
         <div class="peta">
             <h1>Peta Desa Salo Timur</h1>
-          <iframe src="https://goo.gl/maps/acaqDUEUujNHUwj48" width="300" height="150" frameborder="0" style="border:0" name="iframepeta" allowfullscreen></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7548813910566!2d101.00146167410011!3d0.3231019996737199!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d515ca277e7d81%3A0x50d9d753e0634e97!2sKantor%20Kepala%20Desa%20Salo%20Timur!5e0!3m2!1sid!2sid!4v1690189096808!5m2!1sid!2sid" width="300" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div class="kutipan">
           <h1>Kutipan</h1>
