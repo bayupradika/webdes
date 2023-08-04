6ebe100f264f473c0d3c31a28634eb591a139a9c
WalkerVz <ravilmuhammad987@gmail.com>
2023-08-04 14:34:24 +0700

    ex

--- penduduk.php
+++ penduduk.php
@@ -6,7 +6,7 @@ require 'layout/header.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Data Penduduk</title>
 </head>
 <body>
 <div class="container">
@@ -51,7 +51,645 @@ require 'layout/header.php';
             
         </tr>
     </tbody>
-    </table>
+    </table> <br>
+    <h2>B. Data Berdasarkan Usia</h2>
+    <table>
+    <thead>
+        <tr>
+            <th>NO</th>
+            <th>Usia</th>
+            <th>Laki-laki (orang)</th>
+            <th>Perempuan (orang)</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>0-12 bulan</td>
+            <td>17</td>
+            <td>16</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>1 tahun</td>
+            <td>27</td>
+            <td>26</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>2 tahun</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>3 tahun</td>
+            <td>30</td>
+            <td>27</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>4 tahun</td>
+            <td>44</td>
+            <td>37</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>5 tahun</td>
+            <td>53</td>
+            <td>38</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>6 tahun</td>
+            <td>43</td>
+            <td>46</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>7 tahun</td>
+            <td>58</td>
+            <td>41</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>8 tahun</td>
+            <td>47</td>
+            <td>39</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td>9 tahun</td>
+            <td>57</td>
+            <td>50</td>
+        </tr>
+        <tr>
+            <td>11</td>
+            <td>10 tahun</td>
+            <td>49</td>
+            <td>39</td>
+        </tr>
+        <tr>
+            <td>12</td>
+            <td>11 tahun</td>
+            <td>55</td>
+            <td>47</td>
+        </tr>
+        <tr>
+            <td>13</td>
+            <td>12 tahun</td>
+            <td>50</td>
+            <td>44</td>
+        </tr>
+        <tr>
+            <td>14</td>
+            <td>13 tahun</td>
+            <td>45</td>
+            <td>49</td>
+        </tr>
+        <tr>
+            <td>15</td>
+            <td>14 tahun</td>
+            <td>42</td>
+            <td>40</td>
+        </tr>
+        <tr>
+            <td>16</td>
+            <td>15 tahun</td>
+            <td>40</td>
+            <td>49</td>
+        </tr>
+        <tr>
+            <td>17</td>
+            <td>16 tahun</td>
+            <td>42</td>
+            <td>55</td>
+        </tr>
+        <tr>
+            <td>18</td>
+            <td>17 tahun</td>
+            <td>32</td>
+            <td>39</td>
+        </tr>
+        <tr>
+            <td>19</td>
+            <td>18 tahun</td>
+            <td>44</td>
+            <td>33</td>
+        </tr>
+        <tr>
+            <td>20</td>
+            <td>19 tahun</td>
+            <td>33</td>
+            <td>27</td>
+        </tr>
+        <tr>
+            <td>21</td>
+            <td>20 tahun</td>
+            <td>45</td>
+            <td>28</td>
+        </tr>
+        <tr>
+            <td>22</td>
+            <td>21 tahun</td>
+            <td>31</td>
+            <td>34</td>
+        </tr>
+        <tr>
+            <td>23</td>
+            <td>22 tahun</td>
+            <td>38</td>
+            <td>44</td>
+        </tr>
+        <tr>
+            <td>24</td>
+            <td>23 tahun</td>
+            <td>38</td>
+            <td>31</td>
+        </tr>
+        <tr>
+            <td>25</td>
+            <td>24 tahun</td>
+            <td>32</td>
+            <td>28</td>
+        </tr>
+        <tr>
+            <td>26</td>
+            <td>25 tahun</td>
+            <td>29</td>
+            <td>39</td>
+        </tr>
+        <tr>
+            <td>27</td>
+            <td>26 tahun</td>
+            <td>36</td>
+            <td>43</td>
+        </tr>
+        <tr>
+            <td>28</td>
+            <td>27 tahun</td>
+            <td>28</td>
+            <td>23</td>
+        </tr>
+        <tr>
+            <td>29</td>
+            <td>28 tahun</td>
+            <td>50</td>
+            <td>49</td>
+        </tr>
+        <tr>
+            <td>30</td>
+            <td>29 tahun</td>
+            <td>41</td>
+            <td>47</td>
+        </tr>
+        <tr>
+            <td>31</td>
+            <td>30 tahun</td>
+            <td>47</td>
+            <td>34</td>
+        </tr>
+        <tr>
+            <td>32</td>
+            <td>31 tahun</td>
+            <td>43</td>
+            <td>40</td>
+        </tr>
+        <tr>
+            <td>33</td>
+            <td>32 tahun</td>
+            <td>37</td>
+            <td>29</td>
+        </tr>
+        <tr>
+            <td>34</td>
+            <td>33 tahun</td>
+            <td>33</td>
+            <td>49</td>
+        </tr>
+        <tr>
+            <td>35</td>
+            <td>34 tahun</td>
+            <td>40</td>
+            <td>43</td>
+        </tr>
+        <tr>
+            <td>36</td>
+            <td>35 tahun</td>
+            <td>30</td>
+            <td>41</td>
+        </tr>
+        <tr>
+            <td>37</td>
+            <td>36 tahun</td>
+            <td>41</td>
+            <td>39</td>
+        </tr>
+        <tr>
+            <td>38</td>
+            <td>37 tahun</td>
+            <td>36</td>
+            <td>30</td>
+        </tr>
+        <tr>
+            <td>39</td>
+            <td>38 tahun</td>
+            <td>51</td>
+            <td>43</td>
+        </tr>
+        <tr>
+            <td>40</td>
+            <td>39 tahun</td>
+            <td>34</td>
+            <td>38</td>
+        </tr>
+        <tr>
+            <td>41</td>
+            <td>40 tahun</td>
+            <td>42</td>
+            <td>28</td>
+        </tr>
+        <tr>
+            <td>42</td>
+            <td>41 tahun</td>
+            <td>40</td>
+            <td>38</td>
+        </tr>
+        <tr>
+            <td>43</td>
+            <td>42 tahun</td>
+            <td>38</td>
+            <td>4</td>
+        </tr>
+        <tr>
+            <td>44</td>
+            <td>43 tahun</td>
+            <td>33</td>
+            <td>42</td>
+        </tr>
+        <tr>
+            <td>45</td>
+            <td>44 tahun</td>
+            <td>34</td>
+            <td>27</td>
+        </tr>
+        <tr>
+            <td>46</td>
+            <td>45 tahun</td>
+            <td>34</td>
+            <td>27</td>
+        </tr>
+        <tr>
+            <td>47</td>
+            <td>46 tahun</td>
+            <td>25</td>
+            <td>27</td>
+        </tr>
+        <tr>
+            <td>48</td>
+            <td>47 tahun</td>
+            <td>34</td>
+            <td>33</td>
+        </tr>
+        <tr>
+            <td>49</td>
+            <td>48 tahun</td>
+            <td>26</td>
+            <td>19</td>
+        </tr>
+        <tr>
+            <td>50</td>
+            <td>49 tahun</td>
+            <td>30</td>
+            <td>27</td>
+        </tr>
+        <tr>
+            <td>51</td>
+            <td>50 tahun</td>
+            <td>31</td>
+            <td>27</td>
+        </tr>
+        <tr>
+            <td>52</td>
+            <td>51 tahun</td>
+            <td>16</td>
+            <td>16</td>
+        </tr>
+        <tr>
+            <td>53</td>
+            <td>52 tahun</td>
+            <td>19</td>
+            <td>20</td>
+        </tr>
+        <tr>
+            <td>54</td>
+            <td>53 tahun</td>
+            <td>25</td>
+            <td>14</td>
+        </tr>
+        <tr>
+            <td>55</td>
+            <td>54 tahun</td>
+            <td>23</td>
+            <td>23</td>
+        </tr>
+        <tr>
+            <td>56</td>
+            <td>55 tahun</td>
+            <td>17</td>
+            <td>19</td>
+        </tr>
+        <tr>
+            <td>57</td>
+            <td>56 tahun</td>
+            <td>22</td>
+            <td>20</td>
+        </tr>
+        <tr>
+            <td>58</td>
+            <td>57 tahun</td>
+            <td>18</td>
+            <td>13</td>
+        </tr>
+        <tr>
+            <td>59</td>
+            <td>58 tahun</td>
+            <td>25</td>
+            <td>19</td>
+        </tr>
+        <tr>
+            <td>60</td>
+            <td>59 tahun</td>
+            <td>20</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>61</td>
+            <td>60 tahun</td>
+            <td>16</td>
+            <td>13</td>
+        </tr>
+        <tr>
+            <td>62</td>
+            <td>61 tahun</td>
+            <td>18</td>
+            <td>11</td>
+        </tr>
+        <tr>
+            <td>63</td>
+            <td>62 tahun</td>
+            <td>14</td>
+            <td>12</td>
+        </tr>
+        <tr>
+            <td>64</td>
+            <td>63 tahun</td>
+            <td>9</td>
+            <td>9</td>
+        </tr>
+        <tr>
+            <td>65</td>
+            <td>64 tahun</td>
+            <td>5</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>66</td>
+            <td>65 tahun</td>
+            <td>8</td>
+            <td>11</td>
+        </tr>
+        <tr>
+            <td>67</td>
+            <td>66 tahun</td>
+            <td>11</td>
+            <td>13</td>
+        </tr>
+        <tr>
+            <td>68</td>
+            <td>67 tahun</td>
+            <td>1</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>69</td>
+            <td>68 tahun</td>
+            <td>1</td>
+            <td>5</td>
+        </tr>
+        <tr>
+            <td>70</td>
+            <td>69 tahun</td>
+            <td>3</td>
+            <td>4</td>
+        </tr>
+        <tr>
+            <td>71</td>
+            <td>70 tahun</td>
+            <td>2</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>72</td>
+            <td>71 tahun</td>
+            <td>4</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>73</td>
+            <td>72 tahun</td>
+            <td>1</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>74</td>
+            <td>73 tahun</td>
+            <td>2</td>
+            <td>4</td>
+        </tr>
+        <tr>
+            <td>75</td>
+            <td>74 tahun</td>
+            <td>5</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>76</td>
+            <td>Lebih dari 75 tahun</td>
+            <td>33</td>
+            <td>19</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><b>TOTAL</b></td>
+            <td><b>2264</b></td>
+            <td><b>2125</b></td>
+        </tr>
+    </tbody>
+</table>
+    </tbody>
+</table> <br>
+
+<h2>C. Data Berdasarkan Pendidikan</h2>
+    <table>
+    <thead>
+        <tr>
+            <th>Tingkatan Pendidikan</th>
+            <th>Laki-laki</th>
+            <th>Perempuan</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Tamat SD/sederajat</td>
+            <td>180 orang</td>
+            <td>100 orang</td>
+        </tr>
+        <tr>
+            <td>Tamat SMP/sederajat</td>
+            <td>90 orang</td>
+            <td>70 orang</td>
+        </tr>
+        <tr>
+            <td>Tamat SMA/sederajat</td>
+            <td>75 orang</td>
+            <td>70 orang</td>
+        </tr>
+        <tr>
+            <td><strong>Jumlah Total</strong></td>
+            <td><strong>585 orang</strong></td>
+            <td><strong>585 orang</strong></td>
+        </tr>
+    </tbody>
+    </table> <br>
+
+    <h2>C. Data Berdasarkan Pendidikan</h2>
+    <table>
+    <thead>
+        <tr>
+            <th>Jenis Pekerjaan</th>
+            <th>Laki-laki</th>
+            <th>Perempuan</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Petani</td>
+            <td>76 orang</td>
+            <td>23 orang</td>
+        </tr>
+        <tr>
+            <td>Pegawai Negeri Sipil</td>
+            <td>128 orang</td>
+            <td>120 orang</td>
+        </tr>
+        <tr>
+            <td>Peternak</td>
+            <td>4 orang</td>
+            <td>12 orang</td>
+        </tr>
+        <tr>
+            <td>Dokter swasta</td>
+            <td>1 orang</td>
+            <td>2 orang</td>
+        </tr>
+        <tr>
+            <td>Bidan swasta</td>
+            <td>0 orang</td>
+            <td>7 orang</td>
+        </tr>
+        <tr>
+            <td>TNI</td>
+            <td>60 orang</td>
+            <td>1 orang</td>
+        </tr>
+        <tr>
+            <td>POLRI</td>
+            <td>24 orang</td>
+            <td>2 orang</td>
+        </tr>
+        <tr>
+            <td>Dosen swasta</td>
+            <td>1 orang</td>
+            <td>2 orang</td>
+        </tr>
+        <tr>
+            <td>Tukang Batu</td>
+            <td>17 orang</td>
+            <td>6 orang</td>
+        </tr>
+        <tr>
+            <td>Purnawirawan/Pensiunan</td>
+            <td>35 orang</td>
+            <td>15 orang</td>
+        </tr>
+        <tr>
+            <td>Perangkat Desa</td>
+            <td>6 orang</td>
+            <td>2 orang</td>
+        </tr>
+        <tr>
+            <td>Jasa penyewaan peralatan pesta</td>
+            <td>0 orang</td>
+            <td>2 orang</td>
+        </tr>
+        <tr>
+            <td>Tukang Jahit</td>
+            <td>4 orang</td>
+            <td>6 orang</td>
+        </tr>
+        <tr>
+            <td>Tukang Sumur</td>
+            <td>2 orang</td>
+            <td>0 orang</td>
+        </tr>
+        <tr>
+            <td>Karyawan Honorer</td>
+            <td>22 orang</td>
+            <td>20 orang</td>
+        </tr>
+        <tr>
+            <td>Wartawan</td>
+            <td>3 orang</td>
+            <td>0 orang</td>
+        </tr>
+        <tr>
+            <td>Tukang Cukur</td>
+            <td>3 orang</td>
+            <td>0 orang</td>
+        </tr>
+        <tr>
+            <td>Tukang Las</td>
+            <td>2 orang</td>
+            <td>0 orang</td>
+        </tr>
+        <tr>
+            <td>Tukang Listrik</td>
+            <td>5 orang</td>
+            <td>0 orang</td>
+        </tr>
+        <tr>
+            <td>Pemuka Agama</td>
+            <td>6 orang</td>
+            <td>0 orang</td>
+        </tr>
+        <tr>
+            <td>Anggota Legislatif</td>
+            <td>1 orang</td>
+            <td>0 orang</td>
+        </tr>
+        <tr>
+            <td>Satpam/Security</td>
+            <td>5 orang</td>
+            <td>0 orang</td>
+        </tr>
+        <tr>
+            <td><strong>Jumlah Total Penduduk</strong></td>
+            <td><strong>625 orang</strong></td>
+            <td><strong>625 orang</strong></td>
+        </tr>
+    </tbody>
+</table>
 </body>
 </html>
 
